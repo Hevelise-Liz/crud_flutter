ebb1fedaaf8f6395cdcd1dba218bb7c42dfb8dcd
Hevelise - Liz <liz.souza.dias@gmail.com>
2024-07-18 09:11:03 -0300

    feat: primeiro commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const dummy_users = {
+  "1": User(
+      id: "1",
+      name: "Poly",
+      email: "poly@gmail.com",
+      avatarUrl:
+          "https://i.pinimg.com/564x/f0/b5/e4/f0b5e40a2fab5692bb7b39c48d1aea21.jpg"),
+  "2": User(
+      id: "2",
+      name: "Nina",
+      email: "nina@gmail.com",
+      avatarUrl:
+          "https://i.pinimg.com/564x/f0/b5/e4/f0b5e40a2fab5692bb7b39c48d1aea21.jpg"),
+  "3": User(
+      id: "3",
+      name: "Amora",
+      email: "amora@gmail.com",
+      avatarUrl:
+          "https://i.pinimg.com/564x/f0/b5/e4/f0b5e40a2fab5692bb7b39c48d1aea21.jpg"),
+  "4": User(
+      id: "4",
+      name: "Clóvis",
+      email: "clovis@gmail.com",
+      avatarUrl:
+          "https://i.pinimg.com/564x/f0/b5/e4/f0b5e40a2fab5692bb7b39c48d1aea21.jpg"),
+};
